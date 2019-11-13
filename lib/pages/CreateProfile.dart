927b85261acc6689ee0a6cd8bfda8e5d0619a91a
Tomas Torres <tomas3torres@live.com>
2019-11-13 02:37:33 -0600

    added image picker

--- lib/pages/CreateProfile.dart
+++ lib/pages/CreateProfile.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:instant/widgets/GradiantButton.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreateProfile extends StatefulWidget {
   @override
@@ -7,6 +9,15 @@ class CreateProfile extends StatefulWidget {
 }
 
 class _CreateProfileState extends State<CreateProfile> {
+  File profileImage;
+
+  getImage() async{
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+     profileImage = image; 
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,17 +27,73 @@ class _CreateProfileState extends State<CreateProfile> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(5.0),
               child: Text("Your Profile",
                   style: TextStyle(
                       color: Colors.white,
                       fontFamily: 'Montserrat',
                       fontSize: 33)),
             ),
+            GestureDetector(
+              onTap: getImage,
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: profileImage == null
+                            ? AssetImage('assets/default.png')
+                            : FileImage(profileImage),
+                        fit: BoxFit.fill)),
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                TextField(
+                  maxLength: 30,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.grey,
+                      ),
+                      hintText: "Full Name",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none),
+                ),
+                TextField(
+                  maxLength: 30,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.fingerprint,
+                        color: Colors.grey,
+                      ),
+                      hintText: "Username",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none),
+                ),
+                TextField(
+                  maxLength: 200,
+                  minLines: 1,
+                  maxLines: 5,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.language,
+                        color: Colors.grey,
+                      ),
+                      hintText: "Bio",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: InputBorder.none),
+                )
+              ],
+            ),
             GradiantButton(
               text: "Create Profile",
               onTap: () {},
-            )
+            ),
           ],
         ),
       ),
--- lib/utilities/Auth.dart
+++ lib/utilities/Auth.dart
@@ -3,13 +3,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Auth {
   static String uid;
-  static Future<void> sendVerificationCode({
-      String number,
+  static Future<void> sendVerificationCode(
+      {String number,
       Function(AuthCredential) verificationCompleted,
       Function(AuthException) verificationFailed,
-      Function (String, [int]) codeSent,
-      Function (String) codeAutoRetrievalTimeout
-  }) async {
+      Function(String, [int]) codeSent,
+      Function(String) codeAutoRetrievalTimeout}) async {
     FirebaseAuth.instance.verifyPhoneNumber(
         phoneNumber: number,
         timeout: const Duration(seconds: 60),
@@ -19,15 +18,15 @@ class Auth {
         codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
   }
 
-  static Future<bool> signIn(AuthCredential cred) async{
-    try{
-      AuthResult result = await FirebaseAuth.instance.signInWithCredential(cred);
+  static Future<bool> signIn(AuthCredential cred) async {
+    try {
+      AuthResult result =
+          await FirebaseAuth.instance.signInWithCredential(cred);
       uid = result.user.uid;
       return true;
-    }catch(e){
+    } catch (e) {
       return false;
     }
-    
   }
 
   static AuthCredential verifyCode(String smsCode, String verificationId) {
@@ -36,11 +35,25 @@ class Auth {
     return cred;
   }
 
-  static Future<bool> userExist() async{
-    DocumentSnapshot snap = await Firestore.instance.collection('users').document(uid).get();
-    if(snap.exists){
+  static Future<bool> userExist() async {
+    DocumentSnapshot snap =
+        await Firestore.instance.collection('users').document(uid).get();
+    if (snap.exists) {
       return true;
     }
+    return false;
+  }
+
+  static Future<bool> usernameExist(String username) async {
+    QuerySnapshot q = await Firestore.instance
+        .collection('users')
+        .where('username', isEqualTo: username)
+        .getDocuments();
+
+    if (q.documents.length != 0) {
+      return true;
+    }
+
     return false;
   }
 }
