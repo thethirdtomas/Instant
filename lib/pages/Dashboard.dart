748f781fc2ceb872fee66e7f4ac4a5629a7ced90
Tomas Torres <tomas3torres@live.com>
2019-11-21 16:56:43 -0600

    changed appbar font

--- lib/pages/Dashboard.dart
+++ lib/pages/Dashboard.dart
@@ -19,7 +19,7 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Instant", style: TextStyle(fontFamily: 'Montserrat')),
+        title: Text("Instant", style: TextStyle(fontFamily: 'Pacifico')),
         backgroundColor: Colors.black,
         actions: <Widget>[
           IconButton(
