798527103d843d8625a1414b42655072cdeee873
Tomas Torres <tomas3torres@live.com>
2019-11-21 03:07:15 -0600

    dashboard init

--- lib/pages/Dashboard.dart
+++ lib/pages/Dashboard.dart
@@ -10,6 +10,22 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Instant", style:TextStyle(fontFamily: 'Montserrat')),
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: ()=>{},
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        onPressed: (){},
+        child: Icon(Icons.message),
+      ),
       body: Container(
         color: Colors.black,
         child: Center(
