02b5a14b1df2ecec3e6ea1b66b2ace6bbf72d0c0
Tomas Torres <tomas3torres@live.com>
2019-11-21 16:50:29 -0600

    Profile page complete

--- lib/pages/Proile.dart
+++ lib/pages/Proile.dart
@@ -13,8 +13,48 @@ class _ProfileState extends State<Profile> {
     return Scaffold(
       body: Container(
         color: Colors.black,
-        child: Center(
-          child: Text(widget.user['bio'],style: TextStyle(color: Colors.white),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back),
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(widget.user['name'],
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Montserrat',
+                              fontSize: 33)),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(top: 10),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(widget.user['profileImage']),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text("@${widget.user['username']}", style: TextStyle(color:Colors.white, fontFamily: 'Montserrat', fontSize: 20)),
+            Text("${widget.user['bio']}", style: TextStyle(color:Colors.green[100], fontFamily: "Montserrat"))
+          ],
         ),
       ),
     );
