08afd1107ff13c82f33fccedefba5c170195b319
BrBoomr <jrg7_br@hotmail.com>
2019-11-30 01:04:31 -0600

    Registration form no longer overflows. Scrolls when keyboard pops up

--- lib/pages/CreateProfile.dart
+++ lib/pages/CreateProfile.dart
@@ -32,157 +32,163 @@ class _CreateProfileState extends State<CreateProfile> {
     });
   }
 
-  validateProfile() async{
+  validateProfile() async {
     int errorCount = 0;
 
     setState(() {
       loading = true;
       nameError = false;
-      usernameError = false; 
+      usernameError = false;
       imageError = false;
     });
-    if(name.text == ''){
+    if (name.text == '') {
       errorCount += 1;
       nameError = true;
     }
-    if(username.text == ''){
+    if (username.text == '') {
       errorCount += 1;
       usernameError = true;
-    }else if(await FirestoreTask.usernameExist(username.text.replaceAll(' ',''))){
+    } else if (await FirestoreTask.usernameExist(
+        username.text.replaceAll(' ', ''))) {
       errorCount += 1;
       usernameError = true;
       username.clear();
     }
 
-    if(profileImage == null){
+    if (profileImage == null) {
       errorCount += 1;
       imageError = true;
     }
 
-    if(errorCount == 0){
-      if(await createProfile()){
+    if (errorCount == 0) {
+      if (await createProfile()) {
         Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => Dashboard()));
-      }else{
+            context, MaterialPageRoute(builder: (context) => Dashboard()));
+      } else {
         return;
       }
     }
 
     setState(() {
-     loading = false; 
+      loading = false;
     });
   }
 
-  Future<bool> createProfile() async{
+  Future<bool> createProfile() async {
     String imageUrl = await FirestoreTask.uploadImage(profileImage);
-    if(imageUrl == null){
+    if (imageUrl == null) {
       return false;
     }
 
     FirestoreTask.createProfile(
-      name: name.text.trim(),
-      username: username.text.replaceAll(' ',''),
-      bio: bio.text.trim(),
-      profileImage: imageUrl
-    );
+        name: name.text.trim(),
+        username: username.text.replaceAll(' ', ''),
+        bio: bio.text.trim(),
+        profileImage: imageUrl);
     return true;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Text("Your Profile",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Montserrat',
-                      fontSize: 33)),
-            ),
-            GestureDetector(
-              onTap: getImage,
-              child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: imageError?5:0,
-                    color: imageError?Colors.redAccent:Colors.black,
-                  ),
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: profileImage == null
-                          ? AssetImage('assets/default.png')
-                          : FileImage(profileImage),
-                      fit: BoxFit.fill),
-                  ),
+      //Wrapped container with SCSV to void overflow and allow users to scroll when keyboard opens
+      body: SingleChildScrollView(
+        child: Container(
+          //Set height to prevent whitespace from appearing
+          height: MediaQuery.of(context).size.height,
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text("Your Profile",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Montserrat',
+                        fontSize: 33)),
               ),
-            ),
-            Column(
-              children: <Widget>[
-                TextField(
-                  controller: name,
-                  maxLength: 30,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Colors.grey,
-                      ),
-                      suffixIcon: Icon(
-                        Icons.error_outline,
-                        color: nameError ? Colors.redAccent : Colors.black,
-                      ),
-                      hintText: "Full Name",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      border: InputBorder.none),
-                ),
-                TextField(
-                  controller: username,
-                  maxLength: 30,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.fingerprint,
-                        color: Colors.grey,
-                      ),
-                      suffixIcon: Icon(
-                        Icons.error_outline,
-                        color: usernameError ? Colors.redAccent : Colors.black,
-                      ),
-                      hintText: "Username",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      border: InputBorder.none),
-                ),
-                TextField(
-                  controller: bio,
-                  maxLength: 200,
-                  minLines: 1,
-                  maxLines: 5,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.language,
-                        color: Colors.grey,
-                      ),
-                      hintText: "Bio",
-                      hintStyle: TextStyle(color: Colors.grey),
-                      border: InputBorder.none),
-                )
-              ],
-            ),
-            loading
-                ? SpinKitWave(color: Colors.white, size: 30.0)
-                : GradiantButton(
-                    text: "Create Profile",
-                    onTap: validateProfile,
+              GestureDetector(
+                onTap: getImage,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: imageError ? 5 : 0,
+                      color: imageError ? Colors.redAccent : Colors.black,
+                    ),
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: profileImage == null
+                            ? AssetImage('assets/default.png')
+                            : FileImage(profileImage),
+                        fit: BoxFit.fill),
                   ),
-          ],
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  TextField(
+                    controller: name,
+                    maxLength: 30,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Colors.grey,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.error_outline,
+                          color: nameError ? Colors.redAccent : Colors.black,
+                        ),
+                        hintText: "Full Name",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none),
+                  ),
+                  TextField(
+                    controller: username,
+                    maxLength: 30,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.fingerprint,
+                          color: Colors.grey,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.error_outline,
+                          color:
+                              usernameError ? Colors.redAccent : Colors.black,
+                        ),
+                        hintText: "Username",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none),
+                  ),
+                  TextField(
+                    controller: bio,
+                    maxLength: 200,
+                    minLines: 1,
+                    maxLines: 5,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.language,
+                          color: Colors.grey,
+                        ),
+                        hintText: "Bio",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none),
+                  )
+                ],
+              ),
+              loading
+                  ? SpinKitWave(color: Colors.white, size: 30.0)
+                  : GradiantButton(
+                      text: "Create Profile",
+                      onTap: validateProfile,
+                    ),
+            ],
+          ),
         ),
       ),
     );
